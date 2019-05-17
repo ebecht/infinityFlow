63a5f0ea4af8f53eaa7d815bce3a4110e11f580e
ebecht <etienne.becht@protonmail.com>
2019-05-17 11:39:23 -0700

    Train/test set not parallelized to allow multiple models to be trained consistently

--- R/04_svm_regression_all.R
+++ R/04_svm_regression_all.R
@@ -2,12 +2,18 @@
 #' @param x passed from fit_svms
 svm_fitter=function(x){
             try({
-                w=sample(rep(c(TRUE,FALSE),times=c(floor(nrow(x)/2),nrow(x)-floor(nrow(x)/2))))
+                w=x[,"train_set"]==1
                 svm=do.call(function(...){svm(...,x=x[w,chans],y=x[w,yvar])},params)
                 pred=predict(svm,x[,chans])
-                x=cbind(x,SVM=pred,train_set=ifelse(w,1,0))
+                x=cbind(x,SVM=pred)
                 return(list(data=x,svm=svm))
             })
+}
+
+#' Wrapper to XGBoost training. Defined separetely to avoid passing too many objects in parLapplyLB
+#' @param x passed from fit_svms
+xgboost_fitter=function(x){
+    ##WOP
 }
 
 #' Wrapper to SVM predict. Defined separetely to avoid passing too many objects in parLapplyLB
@@ -45,19 +51,17 @@ fit_svms=function(
 
     env=environment()
     
-    ## sapply(
-    ##     c("chans","transforms_chan","transforms_pe"),
-    ##     function(object){
-    ##         assign(object,value=readRDS(file.path(paths["rds"],paste0(object,".Rds"))),envir=env)
-    ##         invisible()
-    ##     }
-    ## )
-    ## events.code=readRDS(file.path(paths["rds"],"pe.Rds"))
-    ## xp=readRDS(file.path(paths["rds"],"xp_transformed_scaled.Rds"))
-
     d.e=split_matrix(xp,events.code)
     rm(xp)
-
+    d.e=lapply(
+        d.e,
+        function(x){
+            w=sample(rep(c(TRUE,FALSE),times=c(floor(nrow(x)/2),nrow(x)-floor(nrow(x)/2))))
+            x=cbind(x,train_set=ifelse(w,1,0))
+            x
+        }
+    )
+    
     clusterEvalQ(
         cl,
         library(e1071)
@@ -76,22 +80,6 @@ fit_svms=function(
     )
 
     stopCluster(cl)
-    
-    ## svms=pbmclapply(
-    ##     d.e,
-    ##     function(x){
-    ##         try({
-    ##             w=sample(rep(c(TRUE,FALSE),times=c(floor(nrow(x)/2),nrow(x)-floor(nrow(x)/2))))
-    ##             svm=do.call(function(...){svm(...,x=x[w,chans],y=x[w,yvar])},params)
-    ##             pred=predict(svm,x[,chans])
-    ##             x=cbind(x,SVM=pred,train_set=ifelse(w,1,0))
-    ##             return(list(data=x,svm=svm))
-    ##         })
-    ##     },
-    ##     mc.cores=cores,
-    ##     mc.preschedule=FALSE,
-    ##     mc.set.seed=TRUE
-    ## )
     saveRDS(svms,file=file.path(paths["rds"],"svms_models.Rds"))
     invisible()
 }
@@ -116,15 +104,6 @@ predict_svms=function(
     mc.reset.stream()
     
     env=environment()
-    ## sapply(
-    ##     c("chans"),
-    ##     function(object){
-    ##         assign(object,value=readRDS(file.path(paths["rds"],paste0(object,".Rds"))),envir=env)
-    ##         invisible()
-    ##     }
-    ## )
-    ## events.code=readRDS(file.path(paths["rds"],"pe.Rds"))
-    ## svms=readRDS(file.path(paths["rds"],"svms_models.Rds"))
     
     xp=lapply(svms,"[[",1)
     xp=lapply(xp,function(x){x[x[,"train_set"]==0,]})
